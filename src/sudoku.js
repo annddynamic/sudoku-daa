effaf498fd7580befff2bc8c812d4e9714f0e425
Nite Qela <nite.qela@student.uni-pr.edu>
2022-06-09 17:46:44 +0200

    implement usedInbox method, fillBox method

--- src/sudoku.js
+++ src/sudoku.js
@@ -35,6 +35,31 @@ const Sudoku = (function () {
         return board
     }
 
+    function unUsedInBox(rowStart, colStart, num, board){
+        for(let i =0; i<3; i++){
+            for(let j =0; j<3; j++){
+                if(board[rowStart+i][colStart+j]===num){
+                    return false
+                }
+            }   
+        }
+        return true
+    }
+
+    function fillBox(row, col, board){
+        let num
+        for(let i =0; i<3; i++){
+            for(let j =0; j<3; j++){
+               do{
+                  num=randomNumberGenerator(GRIDSIZE)  
+               }while(!unUsedInBox(row, col, num, board))
+
+               board[row+i][col+j]= num
+            }   
+        }
+    }
+
+
 })();
 
 export default Sudoku;
